500f4d6b927b743bfad4095612706f2bf7bcdb3b
testervippro <92687973+testervippro@users.noreply.github.com>
2024-12-10 23:37:01 +0700

    Create run.ps1

---
+++ run.ps1
@@ -0,0 +1,7 @@
+docker run --name oracle-19c `
+-p 1521:1521 `
+--env ORACLE_SID=orcl `
+--env ORACLE_PWD=123456789 `
+--env ORACLE_MEM=2000 `
+-v C:\Users\ad\Documents\Oracle\oracle-19c\oradata:/opt/oracle/oradata `
+cuxuanthoai/oracle-19c
